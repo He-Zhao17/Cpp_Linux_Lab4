31afd311adbe0de1157a759d9a06a2dc414f7016
HeZhao_Peko <zh0208@163.com>
2021-03-13 11:30:54 +0800

    # finish without test

--- linked_list.c
+++ linked_list.c
@@ -61,7 +61,7 @@ int insert(LinkedList *list, int item) {
         list->head = itemNode;
         list->size++;
     }
-    return item;
+    return 0;
 }
 
 /** Inserts at a particular position in the list. */
@@ -96,32 +96,138 @@ int insert_at(LinkedList *list, int pos, int item) {
             next = currNode->next->next;
         };
         *(currNode->next) = itemNode;
+        list->size++;
     }
-    return item;
+    return 0;
 }
 
 /** Appends to the end of the list */
 int append(LinkedList *list, item) {
     if (list == NULL) {
+        printf("Error: list is NULL");
+        return -1;
+    }
+    if (list->size  == 0) {
+        if (list->head == NULL) {
+            list->head = new Node{
+                data = item;
+                next = NULL;
+            };
+            list->size++;
+        }
+        else {
+            printf("Error: incorrect list");
+            return -1;
+        }
 
+    }
+    else {
+        Node *currNode = list->head;
+        for (int i = 0; i < list->size - 1; i++) {
+            currNode = currNode->next;
+        }
+        currNode->next = new Node {
+            data = item;
+            next = NULL;
+        };
+        list->size++;
+        return 0;
     }
 }
 
 /** Prints the list contents */
-void print(/* TODO */);
+void print(LinkedList *list) {
+    if (list == NULL) {
+        printf ("Error: list is NULL.");
+        return;
+    }
+    else {
+        if (list->size == 0) {
+            printf("[]");
+        }
+        else {
+            Node *currNode = list->head;
+            for (int i = 0; i < list->size - 1; i++) {
+                printf("[");
+                printf("%d, ", currNode->data);
+                currNode = currNode->next;
+            }
+            printf("%d]", currNode->data);
+        }
+    }
+}
 
 /**
  * Deletes a particular value from the list (if there are duplicate values, the
  * first is deleted. */
-int delete(/* TODO */);
+int delete(LinkedList *list, int item) {
+    if (list == NULL) {
+    printf("Error: list is NULL.");
+    return -1;
+    }
+    if (list->size != 0) {
+        Node *currNode = list->head;
+        if (currNode->data == item) {
+            list->head = currNode->next;
+            list->size--;
+        }
+        for (int i - 0; i < list->size - 1; i++) {
+            if (currNode->next->data == item) {
+                currNode->next = currNode->next->next;
+                list->size--;
+                return 0;
+                break;
+            }
+        }
+        return -1;
+    }
+}
 
 /** Delete the node at a particular position in the list */
-int delete_at(/* TODO */);
+int delete_at(LinkedList *list, int pos) {
+    if (list == NULL) {
+        printf("Error: list is NULL.");
+        return -1;
+    }
+    if (list->size -  1 < pos) {
+        printf("Error: incorrect list.");
+        return -1;
+    }
+    else {
+        Node *currNode = list->head;
+        for (int i = 0; i < pos - 1; i++) {
+            currNode = currNode->next;
+        }
+        currNode->next = currNode->next->next;
+        list->size--;
+        return 0;
+    }
+
+}
 
 /**
  * Locate a particular node. Returns the node index if found, or -1 on failure.
  */
-int search(/* TODO */);
+int search(LinkedList *list, Node *itemNode) {
+    if (list == NULL) {
+        printf("Error: list is NULL.");
+        return -1;
+    } else {
+        if (list->size == 0) {
+            printf("Error: list is NULL.");
+            return -1;
+        }
+        Node *currNode = list->head;
+        for (int i = 0; i < list->size; i++) {
+            if (currNode->next == itemNode->next && currNode->data == itemNode->data) {
+                return i;
+            }
+        }
+
+    }
+
+
+}
 
 // TODO each function should return 0 to indicate success, -1 on failure.
 
